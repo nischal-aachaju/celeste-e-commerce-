9469e96904b92dd8223b23252d97f04af67fd50a
Nischal Shrestha <sthanischal5060@gmail.com>
2026-01-21 00:30:02 +0545

    hero and nav bar fixed

--- src/Components/layout/Navbar.jsx
+++ src/Components/layout/Navbar.jsx
@@ -6,25 +6,24 @@ const Navbar = () => {
   const nav = ["New Arrivals", "Collections", "Accessories", "Editorial"]
 
   return (
-    <div className=' h-14  w-fill flex justify-between pl-4 shadow-[0px_-6px_10px_0px_#525252]'>
-    <div className=' flex justify-center items-center w-fit '><h1 className=' text-2xl font-bold font-serif text-black'>LUXE</h1></div>
-      <div className=' flex w-30 items-center'>
-        <div className=' flex gap-4'>
-          <Search className='cursor-pointer' strokeWidth={1} />
-          <ShoppingBag className='cursor-pointer' strokeWidth={1} />
+    <div className=' sticky top-0 h-14 bg-white  w-full z-100'>
+      <div className=' h-14  w-fill flex justify-between pl-4 shadow-[0px_-6px_10px_0px_#525252]'>
+        <div className=' flex justify-center items-center w-fit '><h1 className=' text-2xl font-bold font-serif text-black'>LUXE</h1></div>
+        <div className=' flex w-30 items-center'>
+          <div className=' flex gap-4'>
+            <Search className='cursor-pointer' strokeWidth={1} />
+            <ShoppingBag className='cursor-pointer' strokeWidth={1} />
+          </div>
+          <div onClick={() => { setMenuBox(!menuBox) }} className=' ml-2 cursor-pointer h-full w-full flex items-center justify-center '>
+            {menuBox ? <TextAlignJustify strokeWidth={1.1} /> : <X strokeWidth={1} size={32} />}
+          </div>
         </div>
-        <div onClick={() => { setMenuBox(!menuBox) }} className=' ml-2 cursor-pointer h-full w-full flex items-center justify-center'>
-          {menuBox ? <TextAlignJustify strokeWidth={1.1} /> : <X strokeWidth={1} size={32} />}
-
-        </div>
+        {menuBox ? null : <nav className=' absolute h-10 w-full top-14 backdrop-blur-sm left-0  overflow-hidden z-100 flex items-center gap-0 '>
+          {nav.map((elem) => {
+            return <a className='bg-white/10 text-white  w-full  overflow-hidden text-shadow-[0px_0px_4px_#000000] border-y  h-14 text-center pt-5  text-[11px] font-extrabold  underline ' href="">{elem}</a>
+          })}
+        </nav>}
       </div>
-      {menuBox ? null : <nav className='absolute h-80 w-full top-14 left-0    z-10 flex flex-col items-end '>
-        {nav.map((elem) => {
-          return <a className='bg-orange-100 w-full h-14 text-center pt-3 text-xl border-b border-white active:underline ' href="">{elem}</a>
-        })}
-      </nav>}
-
-
     </div>
   )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div className='h-[87vh] w-full '>
-
-      
-    </div>
-  )
+    return (
+        <div className='h-[87vh] w-full  bg-[url("/src/assets/images/heroImage.png")] bg-top bg-cover flex items-center '>
+            <div className='h-1/3 w-2/3  text-white pl-4 flex flex-col gap-4'>
+                <div className='text-[12px] text-[#ffffff] w-fit  uppercase backdrop-blur-xs bg-black/10  border border-[#6e581b] rounded-2xl  px-2'>Auttom collection 2026</div>
+                <h2 className=' text-5xl font-serif font-semibold'>Refined <br /><span className='font-normal italic'>Elegance</span></h2>
+                <p className='text-[12px]'>Discover the new standard of luxury with our curated
+                    autumn essentials. Timeless pieces designed for the
+                    modern connoisseur.
+                </p>
+                <div className=' flex gap-2'>
+                    <button className=' bg-white shrink-0 text-black text-sm h-10 w-fit px-4 font-semibold'>SHOP NOW</button>
+                    <button className=' border-white border shrink-0 text-white text-sm h-10 w-fit px-4 font-semibold'>VIEW LOOKBOOK</button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Home
