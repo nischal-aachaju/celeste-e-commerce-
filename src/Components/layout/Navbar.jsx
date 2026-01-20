898b05417acbdd02390d3d0dd7bb3b2a10b0bb61
Nischal Shrestha <sthanischal5060@gmail.com>
2026-01-20 22:09:57 +0545

    nav shadow

--- src/Components/layout/Navbar.jsx
+++ src/Components/layout/Navbar.jsx
@@ -6,19 +6,19 @@ const Navbar = () => {
   const nav = ["New Arrivals", "Collections", "Accessories", "Editorial"]
 
   return (
-    <div className=' h-14  w-fill flex justify-between pl-10'>
-      <div className=' flex justify-center items-center w-fit'><h1 className=' text-2xl font-bold font-serif text-black'>LUXE</h1></div>
+    <div className=' h-14  w-fill flex justify-between pl-4 shadow-[0px_-6px_10px_0px_#525252]'>
+    <div className=' flex justify-center items-center w-fit '><h1 className=' text-2xl font-bold font-serif text-black'>LUXE</h1></div>
       <div className=' flex w-30 items-center'>
         <div className=' flex gap-4'>
           <Search className='cursor-pointer' strokeWidth={1} />
           <ShoppingBag className='cursor-pointer' strokeWidth={1} />
         </div>
         <div onClick={() => { setMenuBox(!menuBox) }} className=' ml-2 cursor-pointer h-full w-full flex items-center justify-center'>
-          {menuBox ? <TextAlignJustify strokeWidth={1.1} /> : <X  strokeWidth={1} size={32} />}
+          {menuBox ? <TextAlignJustify strokeWidth={1.1} /> : <X strokeWidth={1} size={32} />}
 
         </div>
       </div>
-      {menuBox?null:<nav className='absolute h-80 w-full top-14 left-0 border-t   z-10 flex flex-col items-end '>
+      {menuBox ? null : <nav className='absolute h-80 w-full top-14 left-0    z-10 flex flex-col items-end '>
         {nav.map((elem) => {
           return <a className='bg-orange-100 w-full h-14 text-center pt-3 text-xl border-b border-white active:underline ' href="">{elem}</a>
         })}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='h-[87vh] w-full bg-gray-600'>
+    <div className='h-[87vh] w-full '>
+
       
     </div>
   )
