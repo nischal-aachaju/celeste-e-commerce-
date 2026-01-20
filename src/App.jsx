5ca6a903b2fafaab264cb455f8c1b4dde7b2179e
Nischal Shrestha <sthanischal5060@gmail.com>
2026-01-20 20:03:31 +0545

    folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import Collections from './Components/common/hello'
-import Niyam from './Components/common/Niyam'
+import Home from './Pages/Home'
+
+
+
 
 const App = () => {
   return (
     <div className=' h-screen w-full'>
-   {/* <Collections/> */}
-   {/* <Collections /> */}
-   <Niyam />
-      <Niyam />
+      <Home />
+
+
     </div>
   )
 }
