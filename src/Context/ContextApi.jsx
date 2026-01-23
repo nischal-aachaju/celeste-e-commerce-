cbfc3e749f8a300e68f074eabe26d2f6a7cfc8b5
Nischal Shrestha <sthanischal5060@gmail.com>
2026-01-23 17:20:33 +0545

    Api limit to 25

--- src/Context/ContextApi.jsx
+++ src/Context/ContextApi.jsx
@@ -9,7 +9,7 @@ const ContextApi = (props) => {
   useEffect(() => {
     const ApiCall = async () => {
       const response = await axios.get(
-        `https://dummyjson.com/products?limit=30`,
+        `https://dummyjson.com/products?limit=25`,
       );
       setApi(response.data.products);
     };
